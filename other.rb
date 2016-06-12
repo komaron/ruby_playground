2e9190f2eb9b238d0e42f8277274799bb792996d
Komaron James <kjames@jibe.com>
2016-06-11 23:09:26 -0400

    Playing around with inheritance

---
+++ other.rb
@@ -0,0 +1,14 @@
+require 'date'
+require 'time'
+# Class used to demonstarte inheritance
+class Other
+	# FORMAT ME
+	def date
+		Date.today
+	end
+
+	# FORMAT ME
+	def time
+		Time.now
+	end
+end
--- ticket.rb
+++ ticket.rb
@@ -1,20 +1,22 @@
-require 'date'
-class Ticket
+load 'other.rb'
+class Ticket < Other
+	# Constant
+	EVENTS = %w(Movie Play Book Other)
+
+	# Example of simplified getter methods
 	attr_reader :event, :arthor
+	# Examples of simplified get and set methods
 	attr_accessor :price
+
+# Initialize
 	def initialize(event,arthor)
-		@event = event
+		if EVENT.include?(event)
+			@event = event
+		else
+			@event = "UNKNOWN"
+		end
 		@arthor = arthor
 	end
-	def date
-		Date.today
-	end
-
-	def seats
-		"Second Balcony, row J, seat 12"
-	end
-
-
 
 	# An example of sending a method rather thank checking for all
 	# possible combinations, a user will pass what they want and it
@@ -27,4 +29,13 @@ class Ticket
 			"Error I dont know what to do with the request: #{request}"
 		end
 	end
+end
+
+# Example of a singleton method
+# Declare x number of tickets and set prices for each
+# After passing the x number of values the event for the highest
+# price will be returned
+def Ticket.most_expensive(*tickets)
+	ticket = tickets.max_by(&:price)
+	ticket.event
 end
